27a08b78dadf5d698c14fba0708145b4f51886d0
Drebakare <bakaredmlr@gmail.com>
2022-03-11 13:22:06 +0100

    Implementes about us design

--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -11,6 +11,10 @@ import LoginModal from '../../component/LoginModal';
 import ComingSoonModal from '../../component/ComingSoonModal';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { HStack, Box, Button, Text, chakra, VStack,Center } from '@chakra-ui/react';
+import MouseIcon from '../../assets/images/mouse.svg';
+import FooterHero from '../../component/FooterHero';
+
 
 export interface CardProps {
     number?: number;
@@ -61,69 +65,51 @@ const About = () => {
                         handleSignUpOpenModal={handleSignUpOpenModal}
                     />
                 )}
-                <div className='container' data-aos='zoom-in'>
-                    <h1 className='col-lg-11  mx-auto'>
-                        Experience the love for the Round Leather game
-                    </h1>
-                </div>
-            </div>
-            <div className='about-about'>
-                <div className='container d-flex'>
-                    <div className='about-about-left col-lg-6'></div>
-                    <div className='about-about-right col-lg-6 '>
-                        {/* <div className="about-about-right-title" id="about">about sonalysis</div> */}
-                        <h2 className=' about-about-right-header '>
-                            {' '}
-                            <span>Who</span> We Are
-                        </h2>
-                        <div className='about-about-right-text col-lg-8'>
-                            <p>
-                                Sonalysis is our aim at providing/delivering in
-                                real time detailed analytics of a soccer game to
-                                users right from their comfort in Africa and
-                                beyond. We hope to distribute information to
-                                both technical and non-technical users by making
-                                our application user friendly and easily
-                                accessible.
-                            </p>
-                            <p>
-                                Creating a web application that can collate
-                                analytical data from a soccer match that will be
-                                accessible to anyone.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className='container d-flex second'>
-                    <div className='about-about-right col-lg-6  '>
-                        {/* <div className="about-about-right-title" id="about">about sonalysis</div> */}
-                        <h2 className=' about-about-right-header '>
-                            Our <span>Mission</span>{' '}
-                        </h2>
-                        <div className='about-about-right-text col-lg-10 ml-5'>
-                            <p>
-                                Creating a soccer analytics applications that
-                                can compete with the likes of Sportlogiq for
-                                real time analysis of soccer games and real time
-                                delivery.
-                            </p>
-                        </div>
-                    </div>
-                    <div className='about-about-left col-lg-6'>
-                        {/* <img src={StatImg} alt="stat img" /> */}
-                    </div>
-                </div>
-            </div>
+                <Box data-aos="zoom-in" display="flex" alignItems="center" justifyContent="center" >
+                    <VStack mt="5%" w="45%">
+                        <Text pb="10" variant="body">HOME / ABOUT US</Text>
+                        <Text pb="10" variant="header">
+                            <chakra.span color="yellow">
+                                Who&nbsp;
+                            </chakra.span>
+                            We Are
+                        </Text>
+                        <Text pb="10" variant="text">
+                            Sonalysis is our aim at providing/delivering in real time detailed analytics of
+                            a soccer game to users right from their comfort in Africa and beyond. We
+                            hope to distribute information to both technical and non-technical users
+                            by making our application user friendly and easily accessible.
+                            Creating a web application that can collate analytical data from a soccer
+                            match that will be accessible to anyone.
 
-            <div className='about-feature pt-5'>
+                        </Text>
+                        <HStack>
+                            <Button variant="action"> REQUEST A DEMO </Button>
+                            <Button variant="outline"> CONTACT US </Button>
+                        </HStack>
+                        <Box pt="20">
+                            <a href='#team'>
+                                    <img src={MouseIcon} alt='navigate' />
+                            </a>
+                        </Box>
+                    </VStack>
+                </Box>
+            </div>
+           
+            <div className='about-feature pt-5' id="team">
                 <div className='container'>
-                    <h2 className='mx-auto text-center'>
-                        Meet <span>Our Team</span>
-                    </h2>
-                    <p className='text-center'>
-                        We care about what you care about, so we deliever with
-                        you in mind
-                    </p>
+                    <Text textAlign="center" fontSize="3xl" fontWeight="semibold">
+                        Meet&nbsp;
+                        <chakra.span color="yellow">
+                            Our Team
+                        </chakra.span>
+                    </Text>
+                    <Center textAlign="center">
+                        <Text w="60%" variant="text">
+                            Our mission is to crerate a soccer analytics applications that can compete with the likes of Sportlogiq
+                            for real time analysis of soccer games and real time delivery.
+                        </Text>
+                    </Center>
                     <div className='d-lg-flex justify-content-between mt-5'>
                         <div className='team-card'>
                             <div className='team-photo'>
@@ -168,22 +154,7 @@ const About = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='about-game col-lg-10 mx-auto p-5 d-lg-flex align-items-center'>
-                        <div className='about-game-left col-lg-6'>
-                            <h2>Start Analyzing Your Game Today!</h2>
-                            {/* <div className="about-game-left-text mt-5">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tortor, nullam id aliquam.
-              </div> */}
-                        </div>
-                        <div className='col-lg-6'>
-                            <button
-                                onClick={handleSignUpOpenModal}
-                                className='mt-5 mt-lg-0 ml-5'
-                            >
-                                GET STARTED
-                            </button>
-                        </div>
-                    </div>
+                    <FooterHero handleSignUpOpenModal={handleSignUpOpenModal}/>
                 </div>
             </div>
             <Footer
